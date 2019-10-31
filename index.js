9b40267601b0a769058ed64996da0e6e528cd1f7
k-westbrook <kcheriewestbrook@gmail.com>
2019-10-30 21:06:17 -0700

    updated

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const axios = require('axios');
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event) => {
 
   let searchParameters = event.search;
 
-  let results = axios.get(`https://api.yelp.com/v3/businesses/search?categories=restaurant?term=${searchParameters}`);
-
-  console.log(results)
+  let headers = {
+    headers: {
+      Authorization: `Bearer ${process.env.API_KEY}`
+    }
+  }
+  let results;
+  try {
+    results = await axios.get(`https://api.yelp.com/v3/businesses/search?location=Seattle&category=restaurant&term=${searchParameters}&limit=5`, headers);
+  } catch (err) {
+    return err;
+  }
+  return results.data;
 };
