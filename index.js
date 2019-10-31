bcf416b447d13d8afc6ed7b141fd85be92cf8219
k-westbrook <kcheriewestbrook@gmail.com>
2019-10-30 18:03:18 -0700

    updated to test

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const axios = require('axios');
 exports.handler = (event, context, callback) => {
 
   let searchParameters = event.search;
+
+  let results = axios.get(`https://api.yelp.com/v3/businesses/search?categories=restaurant?term=${searchParameters}`);
+
+  console.log(results)
 };
