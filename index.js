c3ba0d71ab87fd5e121c95073bad798e22a133a5
k-westbrook <kcheriewestbrook@gmail.com>
2019-10-30 21:38:38 -0700

    cors

--- index.js
+++ index.js
@@ -9,11 +9,24 @@ exports.handler = async (event) => {
       Authorization: `Bearer ${process.env.API_KEY}`
     }
   }
-  let results;
+  let results, data;
   try {
     results = await axios.get(`https://api.yelp.com/v3/businesses/search?location=Seattle&category=restaurant&term=${searchParameters}&limit=5`, headers);
+    data = {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: {
+
+        message: JSON.stringify('Successful get.'),
+        eventSearchResults: results.data.businesses
+      }
+
+    };
   } catch (err) {
     return err;
   }
-  return results.data.businesses;
+  return data;
 };
